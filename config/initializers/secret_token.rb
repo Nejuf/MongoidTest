b27733d97de820d484af3efb2bbda0c65a0feb00
Nejuf <nejuf1@gmail.com>
2014-02-09 18:36:54 -0800

    Initial setup with Mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongo3::Application.config.secret_token = 'ee2daa521eb2e81aa678f50b49c3d8340ad057103a03f30b35f57e254ee7ad19d6652ac522191a8ba2be1d558896a85891bc9b022c7098cfd53d0764f173a233'
