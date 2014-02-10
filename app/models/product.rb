2c67e619674dbf34ede7d67154dbd818bd9f7fe7
Nejuf <nejuf1@gmail.com>
2014-02-09 20:42:54 -0800

    Adds Product scaffold and validation

---
+++ app/models/product.rb
@@ -0,0 +1,10 @@
+class Product
+  include Mongoid::Document
+  field :name, type: String
+  field :price, type: BigDecimal
+  field :released_on, type: Date
+
+  attr_accessible :name, :price, :released_on
+
+  validates :name, presence: true
+end
